2edc8fac40b4b0d7abdc1d7878d9b14087d2be26
Yaroslav Tomenchyk <yaroslav@webcap.com>
2023-04-21 12:49:55 +0200

    Уроки React JS с нуля / #3 – Знакомство с JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOMClient from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const inputClick = () => console.log('clicked');
+const mouseOver = () => console.log('Mouse Over');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const helpText = "Help Text!";
+
+const elements = (<div>
+  <h1>{helpText}</h1>
+  <input placeholder={helpText} onClick={inputClick} onMouseEnter={mouseOver} />
+  <p>{helpText === "Help Text!" ? "Yes" : "No"}</p>
+</div>)
+
+const app = ReactDOMClient.createRoot(document.getElementById('app'));
+
+app.render(elements);
